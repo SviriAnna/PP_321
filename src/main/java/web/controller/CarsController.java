b312ab04c7672e16101329ea74a606f1ae10f6e0
Anna <ausviridova@gmail.com>
2023-05-02 17:30:33 +0400

    Заменила в контроллере использование мметода дао на использование метода сервиса

--- src/main/java/web/controller/CarsController.java
+++ src/main/java/web/controller/CarsController.java
@@ -1,25 +1,26 @@
 package web.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import web.dao.CarDaoImpl;
+import web.service.CarServiceImpl;
 
 
 @Controller
 public class CarsController {
-    private final CarDaoImpl carDaoImlp;
-
+    private final CarServiceImpl carSericeImpl;
     @Autowired
-    public CarsController(CarDaoImpl carDaoImlp) {
-        this.carDaoImlp = carDaoImlp;
+    public CarsController(CarServiceImpl carSericeImpl) {
+        this.carSericeImpl = carSericeImpl;
     }
 
     @GetMapping("/cars")
-    public String getSomeCars(@RequestParam(value = "count", defaultValue = "5") int count, Model model) {
-        model.addAttribute("cars", carDaoImlp.getSomeCars(count));
+    public String getSomeCars(@RequestParam(defaultValue = "5") int count, Model model) {
+        model.addAttribute("cars", carSericeImpl.getSomeCars(count));
         return "someCars";
     }
 }
--- src/main/java/web/service/CarServiceImpl.java
+++ src/main/java/web/service/CarServiceImpl.java
@@ -1,11 +1,12 @@
 package web.service;
 
+import org.springframework.stereotype.Component;
 import web.dao.CarDao;
 import web.dao.CarDaoImpl;
 import web.model.Car;
 
 import java.util.List;
-
+@Component
 public class CarServiceImpl implements CarService {
 
     CarDao daoCar = new CarDaoImpl();
